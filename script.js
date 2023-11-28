17f08e26052c3db2d76c1d82f2bdd267b4840dc8
scarlett-troncoso <scar.labussieri@gmail.com>
2023-11-29 00:02:00 +0100

    add input and button html add tools js

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ const, let
 
 */
 
+/*
 const kilometri_1 = 0.21 ;
 console.log(kilometri_1);
 
@@ -71,20 +72,42 @@ const result_3 =  user_kilometri * kilometri_1 - sconto_minori; //risultato mino
             let prezzo_numeroElement = document.getElementById ('prezzo_numero')
             prezzo_numeroElement.innerHTML = result_3;
         }
+*/
+
 
 /*
+Descrizione:
+Scrivere un programma che chieda all’utente:
+- Il numero di chilometri da percorrere
+- Età del passeggero Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
+- il prezzo del biglietto è definito in base ai km (0.21 € al km)
+- va applicato uno sconto del 20% per i minorenni
+- va applicato uno sconto del 40% per gli over 65.
+
 MILESTONE 1:
 Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone (non stilizzati), realizziamo le specifiche scritte sopra. La risposta finale (o output) sarà anch’essa da scrivere in console.
+
 MILESTONE 2:
 Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo. Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 Nota:
-Se non vi sentite particolarmente creativi, questa potrebbe essere un’implementazione da seguire per la seconda milestone.
-Potete scegliere di implementare una soluzione completamente diversa oppure simile, ma in ogni caso cercate di farla vostra.
-:saetta: NOTA BENE:
+NOTA BENE:
 Seguite le milestone in ordine, non lavorate alla milestone due se prima non avete fatto funzionare la milestone 1!
 Mi raccomando altrimenti sará tutto piú complicato. Procedete in ordine.
 
+Tools:
+- const / let
+- input / button
+- getElementById / querySelector
+- addEventListener ----- Per far funzionare il button al click
+
 */
+
+// chieda all’utente: Il numero di chilometri da percorrere
+
+
+
+// chieda all’utente: Età del passeggero
+ 
 
 
 
