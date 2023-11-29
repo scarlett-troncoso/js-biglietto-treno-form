5ae7c33f63fd7ad23534501d38f6565173601914
scarlett-troncoso <scar.labussieri@gmail.com>
2023-11-29 18:49:55 +0100

    add calc prezzo_finale

--- script.js
+++ script.js
@@ -1,79 +1,3 @@
-/*
-Descrizione:
-Il programma dovrà chiedere all'utente:
-  il numero di chilometri che vuole percorrere
-  e l'età del passeggero.
-
-Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio.
-Queste sono le regole:
-  il prezzo del biglietto è definito in base ai km (0.21 € al km)
-  va applicato uno sconto del 20% per i minorenni
-  va applicato uno sconto del 40% per gli over 65 (fortunelli).
-
-L'output del prezzo finale va stampata in pagina assicurandoci sia leggibile da un essere umano (massimo due cifre decimali, per indicare centesimi sul prezzo).
-*/
-
-/*
-tools:
-prompt
-const, let
-
-*/
-
-/*
-const kilometri_1 = 0.21 ; --C
-console.log(kilometri_1);
-
-//chiede al utente il numero di chilometri che vuole percorrere
-const user_kilometri = prompt('Quanti Kilometri vuoi percorrere?')
-console.log(user_kilometri)
-
-
-//chiede al utente la sua etá
-const user_eta = prompt('Quanti anni hai?')
-console.log(user_eta)
-
-
-//Variabili risultati e sconti
-const result =  user_kilometri * kilometri_1 ; //risultato biglietto genérico
-const sconto_65 = 40 / result * 100 ; // calcolo sconto over 65
-const sconto_minori = 20 / result * 100 ; // calcolo sconto minoreni
-const result_2 =  user_kilometri * kilometri_1 - sconto_65 ; //risultato over 65
-const result_3 =  user_kilometri * kilometri_1 - sconto_minori; //risultato minoreni
-
-
-    if ((user_eta >= 18) && (user_eta < 65)) {
-    console.log (result); 
-    
-    // Stampa in pagina L'output del prezzo finale
-    let prezzoElement = document.getElementById ('prezzo')
-    prezzo.innerHTML = 'Prezzo del Biglietto:', result;
-
-    let prezzo_numeroElement = document.getElementById ('prezzo_numero')
-    prezzo_numeroElement.innerHTML = result;
-
-        } else if (user_eta >= 65) {
-            console.log (result_2);
-
-            // Stampa in pagina L'output del prezzo finale
-            let prezzoElement = document.getElementById ('prezzo')
-            prezzo.innerHTML = 'Prezzo del Biglietto:', result_2;
-        
-            let prezzo_numeroElement = document.getElementById ('prezzo_numero')
-            prezzo_numeroElement.innerHTML = result_2;
-        
-        } else if (user_eta < 18) {
-            console.log (result_3);
-
-            // Stampa in pagina L'output del prezzo finale
-            let prezzoElement = document.getElementById ('prezzo')
-            prezzo.innerHTML = 'Prezzo del Biglietto:', result_3;
-        
-            let prezzo_numeroElement = document.getElementById ('prezzo_numero')
-            prezzo_numeroElement.innerHTML = result_3;
-        }
-*/
-
 
 /*
 Descrizione:
@@ -117,11 +41,39 @@ console.log('etaInput')
 
 // al click calcolare
 generetorBtn.addEventListener('click', function(){
-    console.log('clicked', kilometriInput.value, etaInput.value);
+    //console.log('clicked', kilometriInput.value, etaInput.value);
     
     const kmValue = kilometriInput.value
     const etaValue = etaInput.value 
-    console.log(kmValue,etaValue);
+    //console.log(kmValue,etaValue,kilometri_1);
+
+    //il prezzo del biglietto è definito in base ai km (0.21 € al km)
+    console.log(kmValue * kilometri_1);
+    const result =  kmValue * kilometri_1 ; //risultato biglietto genérico -- C
+
+    //va applicato uno sconto del 20% per i minorenni
+    let discount;
+
+    if (etaValue < 18) {
+      console.log('apply a 20% discount');
+      discount = 0.2
+
+    } else if (etaValue > 65) {
+      console.log('apply a 40% discount');
+      discount = 0.4
+
+    } else {
+      console.log('apply result');
+      discount = 0
+    }
+
+    console.log(discount);
+
+    const prezzo_finale = result - (result * discount);
+    console.log('Euro' + prezzo_finale.toFixed(2));
 })
+
+
+
 
 
